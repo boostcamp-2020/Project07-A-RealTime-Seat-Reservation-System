269d16ce21b00fcb2e8a65b47046924bb58d6af6
sukstar76 <sukstar76@gmail.com>
2020-11-27 11:06:58 +0900

    fix: 클릭 취소 색깔 변경
    클릭 취소 색깔 변경 및 카운팅 안되는거 수정

--- socket-server/constants/index.ts
+++ socket-server/constants/index.ts
@@ -2,6 +2,7 @@ export enum Color {
   VIP_SEAT = "#B40958",
   R_SEAT = "#DAB8AF",
   S_SEAT = "#6FAACC",
+  UNSOLD_SEAT = "#01DF3A",
   SOLD_SEAT = "#D8D8D8",
   CLICKED_SEAT = "#FA58F4",
   CANCELING_SEAT = "#000000",
