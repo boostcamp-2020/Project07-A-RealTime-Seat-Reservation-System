317140cc99558d1e2f1a9fc0aff7f55cf108a042
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-12-02 01:37:56 +0900

    feat : 결제 페이지 생성

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { SeatSelection, SelectTime } from "./pages";
+import { SeatSelection, SelectTime, Payment } from "./pages";
 
 function App() {
   return (
@@ -8,6 +8,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={SelectTime} />
         <Route path="/seat" component={SeatSelection} />
+        <Route path="/payment" component={Payment} />
       </Switch>
     </BrowserRouter>
   );
