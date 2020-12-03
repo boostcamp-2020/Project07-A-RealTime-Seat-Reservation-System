12cb1895980c35eebc4b61519413d54e8df9ce0b
yellow2041 <37579982+yellow2041@users.noreply.github.com>
2020-12-03 10:57:36 +0900

    fix : 디비 연결 이름 추가

--- api-server/db/mongo.ts
+++ api-server/db/mongo.ts
@@ -4,9 +4,13 @@ const connectMongoDB = () => {
   const mongoHost = (process.env.NODE_ENV === "production"
     ? process.env.MONGO_PRODUCTION_HOST
     : process.env.MONGO_LOCAL_HOST) as string;
-  mongoose.connect(mongoHost, { useUnifiedTopology: true, useNewUrlParser: true }, (err: any) => {
-    console.log(err);
-  });
+  mongoose.connect(
+    mongoHost,
+    { dbName: "project7", useUnifiedTopology: true, useNewUrlParser: true },
+    (err: any) => {
+      console.log(err);
+    },
+  );
 
   const { connection } = mongoose;
   connection.on("error", console.error.bind(console, "connection error:"));
